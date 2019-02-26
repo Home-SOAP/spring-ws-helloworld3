3aeeca3a90f43cd65fb4e3f11ccfabe917a408bd
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-02-26 14:57:18 +0200

    #master

--- src/test/java/com/codenotfound/thread2/TryCathDemo.java
+++ src/test/java/com/codenotfound/thread2/TryCathDemo.java
@@ -3,11 +3,14 @@ package com.codenotfound.thread2;
 public class TryCathDemo {
 
     public static void main(String[] args) {
-        System.out.println( "   >>>>>>>>>>>>>>>> " + func(1) );
-        System.out.println( "   >>>>>>>>>>>>>>>> " + func(0) );
+        System.out.println( "   1 >>>>>>>>>>>>>>>> " + func1(1) );
+        System.out.println( "   1 >>>>>>>>>>>>>>>> " + func1(0) );
+
+        System.out.println( "   2 >>>>>>>>>>>>>>>> " + func2(1) );
+        System.out.println( "   2 >>>>>>>>>>>>>>>> " + func2(0) );
     }
 
-    static boolean func(int param) {
+    static boolean func1(int param) {
         try {
             int result = 1 / param;
             return true;
@@ -16,4 +19,13 @@ public class TryCathDemo {
         }
     }
 
+    static boolean func2(int param) {
+        try {
+            int result = 1 / param;
+            return true;
+        } catch (ArithmeticException ex) {
+
+        }
+        return false;
+    }
 }
